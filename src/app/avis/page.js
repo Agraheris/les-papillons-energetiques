a10d321798b915b567729baaaeefdd60522c6f6b
clement Vigouroux <clement.vigouroux@gmail.com>
2025-05-28 15:28:31 +0200

    add pages

---
+++ src/app/avis/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>Avis</div>
+  )
+}
+
+export default page
---
+++ src/app/prestations/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>Liste des prestations</div>
+  )
+}
+
+export default page
